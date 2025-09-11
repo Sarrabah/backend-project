a16d2720126f1fed89e526ed7a056aa76e0428d7
sarrabah <sarrabenarbia1996@gmail.com>
2025-09-11 10:44:17 +0200

    test: Add the serializer to verify fields

--- create_quote_request/views.py
+++ create_quote_request/views.py
@@ -1,8 +1,15 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from create_quote_request.serializers import QuoteRequestSerializer
+
 
 class QuoteRequestApiView(APIView):
 
     def post(self, request):
-        return Response(request.data, status=201)
+        serializer = QuoteRequestSerializer(data=request.data)
+
+        if serializer.is_valid():
+            return Response(request.data, status=201)
+        
+        return Response(serializer.errors, status=400)
