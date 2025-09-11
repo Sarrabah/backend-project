6bafc614ef75172b70fd9bf3e7bc76b57c83ba2c
sarrabah <sarrabenarbia1996@gmail.com>
2025-09-11 11:43:34 +0200

    reafactor: View refactoring

--- create_quote_request/views.py
+++ create_quote_request/views.py
@@ -10,16 +10,19 @@ class QuoteRequestApiView(APIView):
     def post(self, request):
         serializer = QuoteRequestSerializer(data=request.data)
 
-        if serializer.is_valid():
-            try:
-                new_quote_request = create_quote_request(
-                    request, serializer.validated_data
-                )
-                created_data = QuoteRequestSerializer(new_quote_request).data
-                return Response(created_data, status=201)
-            except Exception as e:
-                return Response(
-                    {"error": f"An unexpected error occurred: {str(e)}"}, status=500
-                )
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=400)
 
-        return Response(serializer.errors, status=400)
+        try:
+            valid_data = serializer.validated_data
+            new_quote_request = create_quote_request(request, valid_data)
+            return self._successful_response_created(new_quote_request)
+        except Exception as e:
+            return Response(
+                {"error": f"An unexpected error occurred: {str(e)}"}, status=500
+            )
+
+    def _successful_response_created(self, new_quote_request):
+        """Retourne la réponse HTTP 201 avec les données sérialisées."""
+        created_data = QuoteRequestSerializer(new_quote_request).data
+        return Response(created_data, status=201)
