2ef58a7407ca086a96c7c356f8b5a10052a3566c
sarrabah <sarrabenarbia1996@gmail.com>
2025-09-11 17:56:39 +0200

    feat: Connection required for users

--- create_quote_request/views.py
+++ create_quote_request/views.py
@@ -1,4 +1,5 @@
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -8,6 +9,7 @@ from create_quote_request.services import create_quote_request
 
 class QuoteRequestApiView(APIView):
     authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def post(self, request):
         serializer = QuoteRequestSerializer(data=request.data)
