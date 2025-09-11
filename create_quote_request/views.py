d114b55fc89271fff8ba7742f65a9c2747f70262
sarrabah <sarrabenarbia1996@gmail.com>
2025-09-11 11:29:55 +0200

    test: Catch th internal error inside the code

--- create_quote_request/views.py
+++ create_quote_request/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from create_quote_request.serializers import QuoteRequestSerializer
+from create_quote_request.services import create_quote_request
 
 
 class QuoteRequestApiView(APIView):
@@ -10,6 +11,15 @@ class QuoteRequestApiView(APIView):
         serializer = QuoteRequestSerializer(data=request.data)
 
         if serializer.is_valid():
-            return Response(request.data, status=201)
-        
+            try:
+                new_quote_request = create_quote_request(
+                    request, serializer.validated_data
+                )
+                created_data = QuoteRequestSerializer(new_quote_request).data
+                return Response(created_data, status=201)
+            except Exception as e:
+                return Response(
+                    {"error": f"An unexpected error occurred: {str(e)}"}, status=500
+                )
+
         return Response(serializer.errors, status=400)
