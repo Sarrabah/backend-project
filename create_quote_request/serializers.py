ef4126828b819a0b321762e34a1192522cd8f11d
Ben Arbia Sarra <123973393+Sarrabah@users.noreply.github.com>
2025-09-11 20:48:25 +0200

    Feature: API Security  (#4)
    * Feature: Establish the authentication with tokens and roles (#3)
    
    * feat: Establish the authentication with tokens and roles
    
    * feat: Connection required for users
    
    * test: Test with authentificated user
    
    * feat: Use rating request

--- create_quote_request/serializers.py
+++ create_quote_request/serializers.py
@@ -4,6 +4,19 @@ from create_quote_request.models import QuoteRequest
 
 
 class QuoteRequestSerializer(serializers.ModelSerializer):
+    STATUS_CHOICES = ["Created", "InProgress", "Completed"]
+    status = serializers.ChoiceField(choices=STATUS_CHOICES)
+
     class Meta:
         model = QuoteRequest
         fields = ["id", "title", "description", "status"]
+
+    def validate_title(self, value):
+        if len(value) == 0:
+            raise serializers.ValidationError("Title cannot be empty.")
+        return value
+
+    def validate_description(self, value):
+        if len(value) == 0:
+            raise serializers.ValidationError("Description cannot be empty.")
+        return value
