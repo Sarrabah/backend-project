02ddc6a8e34b42883f0506736dbdce0ef48db522
Ben Arbia Sarra <123973393+Sarrabah@users.noreply.github.com>
2025-09-13 20:34:03 +0200

    Deploy: Deployment app (#7)
    * feat: use db infos for prod
    
    * refactor: Remove unused link
    
    * chore: Add new dependency to deoly the app
    
    * fix: add staticfiles to be used in the prod
    
    * refactor: Remove comment
    
    * chore: Add dependencies
    
    * chore: Fix versions
    
    * chore: Remove unused dependency

--- backend/settings.py
+++ backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 import sys
 from decouple import config
@@ -28,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-test-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "artcreapro-b8bcfcc5aahfabdc.francecentral-01.azurewebsites.net",]
 
 
 # Application definition
@@ -68,7 +69,7 @@ SWAGGER_SETTINGS = {
             'in': 'header'
         }
     },
-    'USE_SESSION_AUTH': False,  # pour enlever le formulaire username/password
+    'USE_SESSION_AUTH': False,
 }
 
 MIDDLEWARE = [
@@ -79,6 +80,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
 ]
 
 ROOT_URLCONF = "backend.urls"
@@ -112,17 +114,35 @@ if 'test' in sys.argv:
         }
     }
 else:
-    DATABASES = {
-        "default": {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': config('DB_NAME', default='test_db'),
-            'USER': config('DB_USER', default='root'),
-            'PASSWORD': config('DB_PASSWORD', default='root'),
-            'HOST': config('DB_HOST', default='127.0.0.1'),
-            'PORT': config('DB_PORT', default='3306')
-        }
-    }
+    ENV = config("ENV", default="local")
 
+    if ENV == "local":
+        # Base locale MySQL
+        DATABASES = {
+            "default": {
+                'ENGINE': 'django.db.backends.mysql',
+                'NAME': config('DB_NAME'),
+                'USER': config('DB_USER'),
+                'PASSWORD': config('DB_PASSWORD'),
+                'HOST': config('DB_HOST'),
+                'PORT': config('DB_PORT')   
+            }
+        }
+    else:
+        # Base Azure SQL Server (MSSQL)
+        DATABASES = {
+            "default": {
+                'ENGINE': 'mssql',
+                'NAME': config('DB_NAME'),
+                'USER': config('DB_USER'),
+                'PASSWORD': config('DB_PASSWORD'),
+                'HOST': config('DB_HOST'),
+                'PORT': '1433',
+                'OPTIONS': {
+                    'driver': 'ODBC Driver 18 for SQL Server',
+                },
+            }
+        }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -159,7 +179,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
